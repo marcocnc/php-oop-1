1e89d71bed6e87a68c3395e7df0c536d60356fc9
marcocnc <marcocancelliere56@gmail.com>
2023-05-22 14:34:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*Ripassate i primi concetti di classe, variabili e metodi d’istanza che abbiamo visto stamattina e create un file `index.php` in cui:
+ - è definita una **classe ‘Movie’**
+   => all’interno della classe sono dichiarate delle **variabili d’istanza**
+   => all’interno della classe è definito **un costruttore**
+   => all’interno della classe è definito almeno **un metodo**
+- vengono **istanziati almeno due oggetti ‘Movie’** e stampati a schermo i valori delle relative proprietà
+
+
+1. Creare la classe Movie con qualche variabile di istanza (esempio: title, author, type);
+2.
+*/
+
+class Movie {
+    
+
+}
+
+?>
